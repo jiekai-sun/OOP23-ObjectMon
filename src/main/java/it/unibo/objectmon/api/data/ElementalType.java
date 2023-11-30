49498032ed663fd0c806c192915b0fb4773555f5
Xu Jiaqi <jiaqi.xu3@studio.unibo.it>
2023-11-30 14:02:32 +0100

    Added method 'CalcMultiplier' and Modified 'typeMultiplier' to 'getTypeMultiplier'. Also added and modified Javadoc comments

--- src/main/java/it/unibo/objectmon/api/data/ElementalType.java
+++ src/main/java/it/unibo/objectmon/api/data/ElementalType.java
@@ -11,7 +11,8 @@ public enum ElementalType {
     GROUND(6,"Ground"),
     ROCK(7,"Rock"),
     FIGHTING(8,"Fighting"),
-    BUG(9,"Bug");
+    BUG(9,"Bug"),
+    NULL(10,"");
     
     private final int id;
     private final String name;
@@ -21,16 +22,17 @@ public enum ElementalType {
         this.id = id;
         this.name = name;    
         this.typeChart = new Multiplier[][]{
-            new Multiplier[]{Multiplier.E, Multiplier.E, Multiplier.E, Multiplier.E, Multiplier.E, Multiplier.E, Multiplier.E, Multiplier.N, Multiplier.E, Multiplier.E},   //Normal
-            new Multiplier[]{Multiplier.E, Multiplier.N, Multiplier.N, Multiplier.S, Multiplier.N, Multiplier.N, Multiplier.S, Multiplier.S, Multiplier.E, Multiplier.N},   //Grass
-            new Multiplier[]{Multiplier.E, Multiplier.S, Multiplier.N, Multiplier.N, Multiplier.E, Multiplier.E, Multiplier.N, Multiplier.N, Multiplier.E, Multiplier.S},   //Fire
-            new Multiplier[]{Multiplier.E, Multiplier.N, Multiplier.S, Multiplier.N, Multiplier.E, Multiplier.E, Multiplier.S, Multiplier.S, Multiplier.E, Multiplier.E},   //Water
-            new Multiplier[]{Multiplier.E, Multiplier.S, Multiplier.E, Multiplier.E, Multiplier.E, Multiplier.E, Multiplier.E, Multiplier.N, Multiplier.S, Multiplier.S},   //Flying
-            new Multiplier[]{Multiplier.E, Multiplier.S, Multiplier.E, Multiplier.E, Multiplier.E, Multiplier.N, Multiplier.N, Multiplier.N, Multiplier.E, Multiplier.E},   //Poison
-            new Multiplier[]{Multiplier.E, Multiplier.N, Multiplier.S, Multiplier.E, Multiplier.I, Multiplier.S, Multiplier.E, Multiplier.S, Multiplier.E, Multiplier.N},   //Ground
-            new Multiplier[]{Multiplier.E, Multiplier.N, Multiplier.S, Multiplier.N, Multiplier.S, Multiplier.E, Multiplier.N, Multiplier.E, Multiplier.N, Multiplier.S},   //Rock
-            new Multiplier[]{Multiplier.S, Multiplier.E, Multiplier.E, Multiplier.E, Multiplier.N, Multiplier.N, Multiplier.E, Multiplier.S, Multiplier.E, Multiplier.N},   //Fighting
-            new Multiplier[]{Multiplier.E, Multiplier.S, Multiplier.N, Multiplier.E, Multiplier.N, Multiplier.N, Multiplier.E, Multiplier.E, Multiplier.N, Multiplier.E}    //Bug
+            new Multiplier[]{Multiplier.E, Multiplier.E, Multiplier.E, Multiplier.E, Multiplier.E, Multiplier.E, Multiplier.E, Multiplier.N, Multiplier.E, Multiplier.E, Multiplier.E},   //Normal
+            new Multiplier[]{Multiplier.E, Multiplier.N, Multiplier.N, Multiplier.S, Multiplier.N, Multiplier.N, Multiplier.S, Multiplier.S, Multiplier.E, Multiplier.N, Multiplier.E},   //Grass
+            new Multiplier[]{Multiplier.E, Multiplier.S, Multiplier.N, Multiplier.N, Multiplier.E, Multiplier.E, Multiplier.N, Multiplier.N, Multiplier.E, Multiplier.S, Multiplier.E},   //Fire
+            new Multiplier[]{Multiplier.E, Multiplier.N, Multiplier.S, Multiplier.N, Multiplier.E, Multiplier.E, Multiplier.S, Multiplier.S, Multiplier.E, Multiplier.E, Multiplier.E},   //Water
+            new Multiplier[]{Multiplier.E, Multiplier.S, Multiplier.E, Multiplier.E, Multiplier.E, Multiplier.E, Multiplier.E, Multiplier.N, Multiplier.S, Multiplier.S, Multiplier.E},   //Flying
+            new Multiplier[]{Multiplier.E, Multiplier.S, Multiplier.E, Multiplier.E, Multiplier.E, Multiplier.N, Multiplier.N, Multiplier.N, Multiplier.E, Multiplier.E, Multiplier.E},   //Poison
+            new Multiplier[]{Multiplier.E, Multiplier.N, Multiplier.S, Multiplier.E, Multiplier.I, Multiplier.S, Multiplier.E, Multiplier.S, Multiplier.E, Multiplier.N, Multiplier.E},   //Ground
+            new Multiplier[]{Multiplier.E, Multiplier.N, Multiplier.S, Multiplier.N, Multiplier.S, Multiplier.E, Multiplier.N, Multiplier.E, Multiplier.N, Multiplier.S, Multiplier.E},   //Rock
+            new Multiplier[]{Multiplier.S, Multiplier.E, Multiplier.E, Multiplier.E, Multiplier.N, Multiplier.N, Multiplier.E, Multiplier.S, Multiplier.E, Multiplier.N, Multiplier.E},   //Fighting
+            new Multiplier[]{Multiplier.E, Multiplier.S, Multiplier.N, Multiplier.E, Multiplier.N, Multiplier.N, Multiplier.E, Multiplier.E, Multiplier.N, Multiplier.E, Multiplier.E},   //Bug
+            new Multiplier[]{Multiplier.E, Multiplier.E, Multiplier.E, Multiplier.E, Multiplier.E, Multiplier.E, Multiplier.E, Multiplier.E, Multiplier.E, Multiplier.E, Multiplier.E},   //No Type
         };
         
         
@@ -58,7 +60,21 @@ public enum ElementalType {
      * @param defenderType Id of the ElementalType of the defending Objectmon
      * @return Returns the multiplier applied for that type combination
      */
-    public double typeMultiplier(final int attackerType, final int defenderType){
+    public double getTypeMultiplier(final int attackerType, final int defenderType){
         return this.typeChart[attackerType][defenderType].getValue();
+    }
+
+    /**
+     * 
+     * @param moveType ElementalType of the attacking Objectmon's Skill
+     * @param defenderTypes ElementalTypes of the defending Objetmon
+     * @return Returns the effectiveness multiplier of the attacking Skill against the Objectmon's ElementalTypes
+     */
+    public double CalcMultiplier(ElementalType moveType, ElementalType[] defenderTypes){
+        double multiplier = 1;
+            for (ElementalType defenderType : defenderTypes) {
+                multiplier *= getTypeMultiplier(moveType.getId(), defenderType.getId());
+            }
+        return multiplier;
     }
 }
