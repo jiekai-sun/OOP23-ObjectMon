4b507c12f288dae0571d0a246c9bf140e52b0959
Xu Jiaqi <jiaqi.xu3@studio.unibo.it>
2023-12-01 12:55:38 +0100

    Added classes 'Evolution' and 'Objectmon'

---
+++ src/main/java/it/unibo/objectmon/api/data/objectmon/Evolution.java
@@ -0,0 +1,37 @@
+package it.unibo.objectmon.api.data.objectmon;
+
+import java.util.Map;
+import java.util.Optional;
+
+public class Evolution {
+    private final Optional<Map<Integer, Boolean>> isLevel;
+    private final Optional<Boolean> isStone;
+    private final Optional<Boolean> isTrade;
+    
+    public Evolution(final Optional<Map<Integer,Boolean>> isLevel, final Optional<Boolean> isStone, final Optional<Boolean> isTrade) {
+        this.isLevel = isLevel;
+        this.isStone = isStone;
+        this.isTrade = isTrade;
+    }
+
+    public Optional<Map<Integer, Boolean>> getIsLevel() {
+        return isLevel;
+    }
+
+    public Optional<Boolean> getIsStone() {
+        return isStone;
+    }
+
+    public Optional<Boolean> getIsTrade() {
+        return isTrade;
+    }
+
+    public boolean checkEvolution(final int currentLevel){
+        if(getIsLevel().get().containsKey(currentLevel) || getIsStone().isPresent() || getIsTrade().isPresent()){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+} 
---
+++ src/main/java/it/unibo/objectmon/api/data/objectmon/Objectmon.java
@@ -0,0 +1,68 @@
+package it.unibo.objectmon.api.data.objectmon;
+
+import it.unibo.objectmon.api.data.type.*;
+import it.unibo.objectmon.api.data.*;
+import java.util.List;
+import java.util.Optional;
+
+
+
+public class Objectmon {
+    private final int id;
+    private final String name;
+    private final List<ElementalType> types;
+    private final List<Skill> learnList;
+    private final int expOnDeath;
+    private final int maxExp;
+    private final Optional<Evolution> canEvolve;
+
+    private  List<Skill> knownSkills;   
+    private int currentExp;
+
+    public Objectmon(final int id, final String name, final List<ElementalType> types, final List<Skill> learnList, final int expOnDeath, final int maxExp, final Optional<Evolution> canEvolve, final List<Skill> knownSkills) {
+        this.id = id;
+        this.name = name;
+        this.types = types;
+        this.learnList = learnList;
+        this.expOnDeath = expOnDeath;
+        this.maxExp = maxExp;
+        this.canEvolve = canEvolve;
+        this.knownSkills = knownSkills;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getCurrentExp() {
+        return currentExp;
+    }
+
+    public int getMaxExp() {
+        return maxExp;
+    }
+
+    public List<Skill> getKnownSkills() {
+        return knownSkills;
+    }
+
+    public Optional<Evolution> getCanEvolve() {
+        return canEvolve;
+    }
+
+    public int getExpOnDeath() {
+        return expOnDeath;
+    }
+
+    public List<Skill> getLearnList() {
+        return learnList;
+    }
+
+    public List<ElementalType> getTypes() {
+        return types;
+    }
+}
